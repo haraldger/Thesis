22c75e128aa75e76b4458f1307f881acf3e4868d
Harald Gerhardsen <k1889292@kcl.ac.uk>
2022-04-04 17:19:16 +0800

    Add effect classes and effect methods to AIDomainBuilder for workers and building spots.

--- Assets/Scripts/AI/Domains/AIDomainBuilder.cs
+++ Assets/Scripts/AI/Domains/AIDomainBuilder.cs
@@ -50,5 +50,45 @@ public class AIDomainBuilder : BaseDomainBuilder<AIDomainBuilder, AIContext, int
         return this;
     }
 
+    public AIDomainBuilder MakeWorkerBusy(EffectType type = EffectType.PlanAndExecute)
+    {
+        if (Pointer is IPrimitiveTask<int> task)
+        {
+            var effect = new MakeWorkerBusyEffect(type);
+            task.AddEffect(effect);
+        }
+        return this;
+    }
+
+    public AIDomainBuilder MakeWorkerIdle(EffectType type = EffectType.PlanAndExecute)
+    {
+        if (Pointer is IPrimitiveTask<int> task)
+        {
+            var effect = new MakeWorkerIdleEffect(type);
+            task.AddEffect(effect);
+        }
+        return this;
+    }
+
+    public AIDomainBuilder MakeBuildingSpotFree(EffectType type = EffectType.PlanAndExecute)
+    {
+        if (Pointer is IPrimitiveTask<int> task)
+        {
+            var effect = new MakeBuildingSpotFreeEffect(type);
+            task.AddEffect(effect);
+        }
+        return this;
+    }
+
+    public AIDomainBuilder MakeBuildingSpotOccupied(EffectType type = EffectType.PlanAndExecute)
+    {
+        if (Pointer is IPrimitiveTask<int> task)
+        {
+            var effect = new MakeBuildingSpotOccupiedEffect(type);
+            task.AddEffect(effect);
+        }
+        return this;
+    }
+
 }
 
